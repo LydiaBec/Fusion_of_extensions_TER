d348588b1d5be7793c616581b1c6f6675c10f3e5
LydiaBec <80390636+LydiaBec@users.noreply.github.com>
2021-05-14 12:31:06 +0200

    Update Distance.java

--- src/fusion/Distance.java
+++ src/fusion/Distance.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fusion;
 
 import java.util.Collection;
-
+//Abstract Distance function
 public abstract class Distance {
 	public abstract int choosenDistance(Collection<String> Ext, Collection<String> Mod);
 }
