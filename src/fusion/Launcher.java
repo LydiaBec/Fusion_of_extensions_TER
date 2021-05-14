57c788e8934ac81a0f85421596734e4892327ae1
LydiaBec <80390636+LydiaBec@users.noreply.github.com>
2021-05-14 20:51:29 +0200

    Update Launcher.java

--- src/fusion/Launcher.java
+++ src/fusion/Launcher.java
@@ -90,16 +90,32 @@ public class Launcher {
 		Collection<Collection<String>> model = ReadingFiles
 				.transformeModeles(ReadingFiles.readingConstrainte(args_d.get(1)));
 		Models mod = new Models(model);
-		if (mod.getModels().isEmpty()) {
-			System.err.println("Unsatisfiable constrainte");
-
-		} else {
-			String sem = new String();
-			Vector<String> fileName = ReadingFiles.nameFile(args_d.get(0));
+		String sem = new String();
+		
+		Vector<String> fileName = ReadingFiles.nameFile(args_d.get(0));
 			boolean supported = true;
 			int j = 0;
+		
+			if(mod.getModels().isEmpty()) { // Si il n'y a pas de modele ou pas de modele.
 			for (DungAF af : ReadingFiles.Lectures(args_d.get(0))) {
-
+				if(addMod){
+                    			 model = toCollec(af.getArguments());
+                   			 mod = new Models(model);
+                   			 addMod = false;
+               				 } 
+               		 mainFunc(fileName, af, mod, sem, distance, supported, j);
+               		 j++;
+            		}
+            		mainAgregate(mod, as, supported);
+			}else { // si il y a un modele
+				 for (DungAF af : ReadingFiles.Lectures(args_d.get(0))) {
+                mainFunc(fileName, af, mod, sem, distance, supported, j);
+                j++;
+            }
+            mainAgregate(mod, as, supported);
+        }
+    }
+		    public static void mainFunc(Vector<String> fileName, DungAF af, Models mod, String sem, Distance distance, boolean supported, int j){
 				if (fileName.get(j).toString().endsWith("co")) {
 					sem = "CO";
 				} else {
@@ -114,7 +130,7 @@ public class Launcher {
 
 							} else {
 								if (fileName.get(j).toString().endsWith("txt")
-										|| fileName.get(j).toString().endsWith("tgf")||fileName.get(j).toString().endsWith("apx")) {
+										|| fileName.get(j).toString().endsWith("tgf")) {
 									sem = "CO";
 								} else {
 									System.err.println("file extension not supported : " + fileName.get(j).toString()
@@ -131,8 +147,9 @@ public class Launcher {
 
 				// calculating distance
 				CalculDistance.calculDistance(af, mod, distance, sem);
-				j++;
 			}
+		    public static void mainAgregate(Models mod, Aggregate_Function as, boolean supported){
+
 			if (supported) {
 				resultat = as.choosenAggregate(mod);
 				System.out.println("Distance final apres aggregation = " + resultat);
@@ -145,6 +162,7 @@ public class Launcher {
 			}
 
 		}
+	}
 		long tempsFin = System.nanoTime(); 
 		double seconds = (tempsFin - tempsDebut) / 1e9; 
 		System.out.println();
