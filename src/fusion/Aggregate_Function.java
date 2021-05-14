8a0c29568deb0b9486922bd57340896b13992f8b
LydiaBec <80390636+LydiaBec@users.noreply.github.com>
2021-05-14 12:30:21 +0200

    Update Aggregate_Function.java

--- src/fusion/Aggregate_Function.java
+++ src/fusion/Aggregate_Function.java
@@ -1,6 +1,6 @@
 package fusion;
 import java.util.Vector;
-
+//Abstract aggregation function
 public  abstract class Aggregate_Function {
 	public abstract Vector<Float> choosenAggregate(Models mod);
 }
